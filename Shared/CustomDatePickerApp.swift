623267932505daf2b49c1967d2bec6dc911fd4a0
Mohammad Nazib -ul- Islam <nzbszb@gmail.com>
2021-01-07 10:38:34 +0600

    Initial Commit

---
+++ Shared/CustomDatePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomDatePickerApp.swift
+//  Shared
+//
+//  Created by Admin on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomDatePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
